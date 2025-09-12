88dc121ab104e8db2616111612d0aae68493551a
Marc Philipp <mail@marcphilipp.de>
2025-09-12 18:16:55 +0200

    Do not report breaking changes for new classes

--- gradle/plugins/japicmp/src/main/kotlin/junitbuild/japicmp/UnacceptedIncompatibilityRule.kt
+++ gradle/plugins/japicmp/src/main/kotlin/junitbuild/japicmp/UnacceptedIncompatibilityRule.kt
@@ -1,7 +1,9 @@
 package junitbuild.japicmp
 
+import japicmp.model.JApiBehavior
 import japicmp.model.JApiClass
 import japicmp.model.JApiCompatibility
+import japicmp.model.JApiField
 import japicmp.model.JApiImplementedInterface
 import me.champeau.gradle.japicmp.report.Violation
 import me.champeau.gradle.japicmp.report.stdrules.AbstractRecordingSeenMembers
@@ -20,10 +22,22 @@ class UnacceptedIncompatibilityRule(params: Map<String, String>): AbstractRecord
             // The changes about the interface's methods will be reported already
             return null
         }
+        if (isInNewClass(element)) {
+            return null
+        }
         return if (!element.isBinaryCompatible) {
             Violation.notBinaryCompatible(element, severitySource.determineSeverity(element))
         } else if (!element.isSourceCompatible) {
             Violation.any(element, severitySource.determineSeverity(element), "Is not source compatible")
         } else null
+    }
+
+    private fun isInNewClass(element: JApiCompatibility): Boolean {
+        return when (element) {
+            is JApiClass -> element.oldClass.isEmpty
+            is JApiBehavior -> isInNewClass(element.getjApiClass())
+            is JApiField -> isInNewClass(element.getjApiClass())
+            else -> false
+        }
     }
 }
