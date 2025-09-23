38e1caae20ac63e5a09f6b48efcf4cc3b4650ebb
Marc Philipp <mail@marcphilipp.de>
2025-09-23 10:18:18 +0200

    Move entries to 5.14 list after backporting

--- platform-tooling-support-tests/projects/graalvm-starter/build.gradle.kts
+++ platform-tooling-support-tests/projects/graalvm-starter/build.gradle.kts
@@ -29,12 +29,14 @@ tasks.test {
 
 val initializeAtBuildTime = mapOf(
 	// These need to be added to native-build-tools
-	"6.0" to listOf(
-		"org.junit.platform.commons.logging.LoggerFactory\$DelegatingLogger",
+	"5.14" to listOf(
 		"org.junit.platform.commons.util.DefaultClasspathScanner",
 		"org.junit.platform.commons.util.KotlinReflectionUtils",
 		"org.junit.platform.launcher.core.DiscoveryIssueNotifier\$1",
 		"org.junit.platform.launcher.core.HierarchicalOutputDirectoryCreator",
+	)
+	"6.0" to listOf(
+		"org.junit.platform.commons.logging.LoggerFactory\$DelegatingLogger",
 	)
 )
 
