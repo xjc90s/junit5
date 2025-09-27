12ae063311ceba2c038bc4c80fb357eec8f10f63
Marc Philipp <mail@marcphilipp.de>
2025-09-27 14:05:57 +0200

    Allow new classes to be added

--- gradle/plugins/japicmp/src/main/kotlin/junitbuild/japicmp/AcceptedViolationSuppressor.kt
+++ gradle/plugins/japicmp/src/main/kotlin/junitbuild/japicmp/AcceptedViolationSuppressor.kt
@@ -1,6 +1,9 @@
 package junitbuild.japicmp
 
+import japicmp.model.JApiBehavior
 import japicmp.model.JApiClass
+import japicmp.model.JApiCompatibility
+import japicmp.model.JApiField
 import japicmp.model.JApiImplementedInterface
 import me.champeau.gradle.japicmp.report.Severity.accepted
 import me.champeau.gradle.japicmp.report.Violation
@@ -20,6 +23,8 @@ class AcceptedViolationSuppressor(params: Map<String, String>) : ViolationTransf
     private fun Violation.isRedundant(): Boolean =
         // The changes about the interface's methods will be reported already
         member is JApiImplementedInterface
+                // Allow new classes to be added
+                || member.isInNewClass()
                 // A member of the class breaks binary or source compatibility and will be reported
                 || (member is JApiClass && member.compatibilityChanges.none { !it.isBinaryCompatible || !it.isSourceCompatible } && !member.isBreakingSuperClassChange())
 
@@ -27,4 +32,12 @@ class AcceptedViolationSuppressor(params: Map<String, String>) : ViolationTransf
         acceptedIncompatibilities.contains(member.fullyQualifiedName)
                 || acceptedIncompatibilities.contains(member.fullyQualifiedClassName)
 
+    private fun JApiCompatibility.isInNewClass(): Boolean {
+        return when (this) {
+            is JApiClass -> oldClass.isEmpty
+            is JApiBehavior -> getjApiClass().isInNewClass()
+            is JApiField -> getjApiClass().isInNewClass()
+            else -> false
+        }
+    }
 }
