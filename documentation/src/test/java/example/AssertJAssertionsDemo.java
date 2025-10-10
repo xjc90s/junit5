0403116e5bd15909f92f8a00341ee9dc4f40cc38
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-10-10 14:49:37 +0200

    Replace Hamcrest example with AssertJ in User Guide
    This commit also adds a "tip" about using AssertJ's Assumptions support.
    
    Closes #5050

--- documentation/src/test/java/example/AssertJAssertionsDemo.java
+++ documentation/src/test/java/example/AssertJAssertionsDemo.java
@@ -11,21 +11,19 @@
 package example;
 
 // tag::user_guide[]
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import example.util.Calculator;
 
 import org.junit.jupiter.api.Test;
 
-class HamcrestAssertionsDemo {
+class AssertJAssertionsDemo {
 
 	private final Calculator calculator = new Calculator();
 
 	@Test
-	void assertWithHamcrestMatcher() {
-		assertThat(calculator.subtract(4, 1), is(equalTo(3)));
+	void assertWithAssertJ() {
+		assertThat(calculator.subtract(4, 1)).isEqualTo(3);
 	}
 
 }
