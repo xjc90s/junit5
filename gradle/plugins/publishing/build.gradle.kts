348b0343cd5613d9520c71618aee3c9a18ea8cc8
Marc Philipp <mail@marcphilipp.de>
2025-12-05 19:48:37 +0100

    Work around CVE-2025-66516

--- gradle/plugins/publishing/build.gradle.kts
+++ gradle/plugins/publishing/build.gradle.kts
@@ -16,6 +16,14 @@ dependencies {
 			because("Workaround for CVE-2025-4949")
 		}
 	}
+	constraints {
+		implementation("org.apache.tika:tika-core") {
+			version {
+				require("3.2.2")
+			}
+			because("Workaround for CVE-2025-66516")
+		}
+	}
 }
 
 tasks.compileJava {
