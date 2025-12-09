9115b1db7b8c6934eb3951788f875bea4fd424b1
Marc Philipp <mail@marcphilipp.de>
2025-12-09 20:06:08 +0100

    Work around CVEs reported against build dependencies

--- gradle/plugins/antora/build.gradle.kts
+++ gradle/plugins/antora/build.gradle.kts
@@ -8,7 +8,23 @@ plugins {
 dependencies {
 	implementation(projects.buildParameters)
 	implementation(libs.plugins.node.markerCoordinates)
+	constraints {
+		implementation("com.fasterxml.jackson.core:jackson-core") {
+			version {
+				require("2.15.0")
+			}
+			because("Workaround for CVE-2025-52999")
+		}
+	}
 	implementation(libs.plugins.spring.antora.markerCoordinates)
+	constraints {
+		implementation("org.yaml:snakeyaml") {
+			version {
+				require("2.0")
+			}
+			because("Workaround for CVE-2022-1471")
+		}
+	}
 }
 
 tasks.compileJava {
