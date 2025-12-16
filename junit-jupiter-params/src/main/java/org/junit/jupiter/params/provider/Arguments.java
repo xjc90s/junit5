83de981070a0881664dbd0925eaf374a3972111f
mariokhoury4 <mariokhoury4@outlook.com>
2025-12-16 03:59:30 +0100

    Add List-based support to Arguments API (#4574)
    These additions make it easier to dynamically build arguments from 
    collections when using @ParameterizedTest.
    
    Fixes: #4535
    
    Co-authored-by: Marc Philipp <mail@marcphilipp.de>
    Co-authored-by: M.P. Korstanje <rien.korstanje@gmail.com>

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/provider/Arguments.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/provider/Arguments.java
@@ -10,8 +10,14 @@
 
 package org.junit.jupiter.params.provider;
 
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 import static org.apiguardian.api.API.Status.MAINTAINED;
 import static org.apiguardian.api.API.Status.STABLE;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import org.apiguardian.api.API;
 import org.jspecify.annotations.Nullable;
@@ -26,10 +32,11 @@ import org.junit.platform.commons.util.Preconditions;
  *
  * @apiNote <p>This interface is specifically designed as a simple holder of
  * arguments for a parameterized test. Therefore, if you end up
- * {@linkplain java.util.stream.Stream#map(java.util.function.Function) transforming} or
+ * {@linkplain java.util.stream.Stream#map(java.util.function.Function) transforming}
+ * or
  * {@linkplain java.util.stream.Stream#filter(java.util.function.Predicate) filtering}
- * the arguments, you should consider using one of the following in intermediate
- * steps:
+ * the arguments, you should consider using one of the following in
+ * intermediate steps:
  *
  * <ul>
  *   <li>The standard Java collections</li>
@@ -58,13 +65,31 @@ public interface Arguments {
 	 * Get the arguments used for an invocation of the
 	 * {@code @ParameterizedTest} method.
 	 *
-	 * @apiNote If you need a type-safe way to access some or all of the arguments,
-	 * please read the {@linkplain Arguments class-level API note}.
+	 * @apiNote If you need a type-safe way to access some or all of the
+	 * arguments, please read the {@linkplain Arguments class-level API note}.
 	 *
 	 * @return the arguments; never {@code null} but may contain {@code null}
 	 */
 	@Nullable
 	Object[] get();
+
+	/**
+	 * Convert the arguments to a new mutable {@link List} containing the same
+	 * elements as {@link #get()}.
+	 *
+	 * <p>This is useful for test logic that benefits from {@code List}
+	 * operations such as filtering, transformation, or assertions.
+	 *
+	 * @return a mutable List of arguments; never {@code null} but may contain
+	 * {@code null}
+	 * @since 6.1
+	 */
+	@API(status = EXPERIMENTAL, since = "6.1")
+	default List<@Nullable Object> toList() {
+		// We could return List<?> here but the unbounded wildcard is painful
+		// to work with.
+		return new ArrayList<>(Arrays.asList(get()));
+	}
 
 	/**
 	 * Factory method for creating an instance of {@code Arguments} based on
@@ -73,12 +98,35 @@ public interface Arguments {
 	 * @param arguments the arguments to be used for an invocation of the test
 	 * method; must not be {@code null} but may contain {@code null}
 	 * @return an instance of {@code Arguments}; never {@code null}
+	 * @see #from(Iterable)
 	 * @see #arguments(Object...)
 	 * @see #argumentSet(String, Object...)
 	 */
 	static Arguments of(@Nullable Object... arguments) {
 		Preconditions.notNull(arguments, "arguments array must not be null");
 		return () -> arguments;
+	}
+
+	/**
+	 * Factory method for creating an instance of {@code Arguments} based on
+	 * the supplied {@link Iterable} of {@code arguments}.
+	 *
+	 * <p>The iterable supplied to this method should be a finite collection
+	 * and have a reliable iteration order to provide arguments in a consistent
+	 * order to tests. It is therefore recommended that the iterable be a
+	 * {@link java.util.SequencedCollection} (on Java 21 or higher),
+	 * {@link java.util.List}, or similar.
+	 *
+	 * @param arguments the arguments to be used for an invocation of the test
+	 * method; must not be {@code null} but may contain {@code null}
+	 * @return an instance of {@code Arguments}; never {@code null}
+	 * @since 6.1
+	 * @see #argumentsFrom(Iterable)
+	 */
+	@API(status = EXPERIMENTAL, since = "6.1")
+	static Arguments from(Iterable<?> arguments) {
+		Preconditions.notNull(arguments, "arguments must not be null");
+		return of(toArray(arguments));
 	}
 
 	/**
@@ -94,9 +142,36 @@ public interface Arguments {
 	 * @return an instance of {@code Arguments}; never {@code null}
 	 * @since 5.3
 	 * @see #argumentSet(String, Object...)
+	 * @see #argumentsFrom(Iterable)
 	 */
 	static Arguments arguments(@Nullable Object... arguments) {
 		return of(arguments);
+	}
+
+	/**
+	 * Factory method for creating an instance of {@code Arguments} based on
+	 * the supplied {@link Iterable} of {@code arguments}.
+	 *
+	 * <p>This method is an <em>alias</em> for {@link Arguments#from} and is
+	 * intended to be used when statically imported &mdash; for example, via:
+	 * {@code import static org.junit.jupiter.params.provider.Arguments.argumentsFrom;}
+	 *
+	 * <p>The iterable supplied to this method should be a finite collection
+	 * and have a reliable iteration order to provide arguments in a consistent
+	 * order to tests. It is therefore recommended that the iterable be a
+	 * {@link java.util.SequencedCollection} (on Java 21 or higher),
+	 * {@link java.util.List}, or similar.
+	 *
+	 * @param arguments the arguments to be used for an invocation of the test
+	 * method; must not be {@code null} but may contain {@code null}
+	 * @return an instance of {@code Arguments}; never {@code null}
+	 * @since 6.1
+	 * @see #arguments(Object...)
+	 * @see #argumentSetFrom(String, Iterable)
+	 */
+	@API(status = EXPERIMENTAL, since = "6.1")
+	static Arguments argumentsFrom(Iterable<?> arguments) {
+		return from(arguments);
 	}
 
 	/**
@@ -104,25 +179,72 @@ public interface Arguments {
 	 * {@code name} and {@code arguments}.
 	 *
 	 * <p>Favor this method over {@link Arguments#of Arguments.of(...)} and
-	 * {@link Arguments#arguments arguments(...)} when you wish to assign a name
-	 * to the entire set of arguments.
+	 * {@link Arguments#arguments arguments(...)} when you wish to assign a
+	 * name to the entire set of arguments.
 	 *
 	 * <p>This method is well suited to be used as a static import &mdash; for
 	 * example, via:
 	 * {@code import static org.junit.jupiter.params.provider.Arguments.argumentSet;}.
 	 *
-	 * @param name the name of the argument set; must not be {@code null} or blank
+	 * @param name the name of the argument set; must not be {@code null} or
+	*  blank
 	 * @param arguments the arguments to be used for an invocation of the test
 	 * method; must not be {@code null} but may contain {@code null}
 	 * @return an {@code ArgumentSet}; never {@code null}
 	 * @since 5.11
 	 * @see ArgumentSet
-	 * @see org.junit.jupiter.params.ParameterizedTest#ARGUMENT_SET_NAME_PLACEHOLDER
-	 * @see org.junit.jupiter.params.ParameterizedTest#ARGUMENT_SET_NAME_OR_ARGUMENTS_WITH_NAMES_PLACEHOLDER
+	 * @see #argumentSetFrom(String, Iterable)
+	 * @see org.junit.jupiter.params.ParameterizedInvocationConstants#ARGUMENT_SET_NAME_PLACEHOLDER
+	 * @see org.junit.jupiter.params.ParameterizedInvocationConstants#ARGUMENT_SET_NAME_OR_ARGUMENTS_WITH_NAMES_PLACEHOLDER
 	 */
 	@API(status = MAINTAINED, since = "5.13.3")
 	static ArgumentSet argumentSet(String name, @Nullable Object... arguments) {
 		return new ArgumentSet(name, arguments);
+	}
+
+	/**
+	 * Factory method for creating an {@link ArgumentSet} based on the supplied
+	 * {@code name} and {@link Iterable} of {@code arguments}.
+	 *
+	 * <p>Favor this method over {@link Arguments#from(Iterable) Arguments.from(...)} and
+	 * {@link Arguments#argumentsFrom(Iterable) argumentsFrom(...)} when you wish to assign a
+	 * name to the entire set of arguments.
+	 *
+	 * <p>This method is well suited to be used as a static import &mdash; for
+	 * example, via:
+	 * {@code import static org.junit.jupiter.params.provider.Arguments.argumentSetFrom;}.
+	 *
+	 * <p>The iterable supplied to this method should be a finite collection
+	 * and have a reliable iteration order to provide arguments in a consistent
+	 * order to tests. It is therefore recommended that the iterable be a
+	 * {@link java.util.SequencedCollection} (on Java 21 or higher),
+	 * {@link java.util.List}, or similar.
+	 *
+	 * @param name the name of the argument set; must not be {@code null}
+	 * or blank
+	 * @param arguments the arguments to be used for an invocation of the test
+	 * method; must not be {@code null} but may contain {@code null}
+	 * @return an {@code ArgumentSet}; never {@code null}
+	 * @since 6.1
+	 * @see ArgumentSet
+	 * @see #argumentSet(String, Object...)
+	 * @see org.junit.jupiter.params.ParameterizedInvocationConstants#ARGUMENT_SET_NAME_PLACEHOLDER
+	 * @see org.junit.jupiter.params.ParameterizedInvocationConstants#ARGUMENT_SET_NAME_OR_ARGUMENTS_WITH_NAMES_PLACEHOLDER
+	 */
+	@API(status = EXPERIMENTAL, since = "6.1")
+	static ArgumentSet argumentSetFrom(String name, Iterable<?> arguments) {
+		Preconditions.notBlank(name, "name must not be null or blank");
+		Preconditions.notNull(arguments, "arguments list must not be null");
+		return argumentSet(name, toArray(arguments));
+	}
+
+	private static Object[] toArray(Iterable<?> arguments) {
+		if (arguments instanceof Collection<?> collection) {
+			return collection.toArray();
+		}
+		var collection = new ArrayList<>();
+		arguments.forEach(collection::add);
+		return collection.toArray();
 	}
 
 	/**
@@ -131,8 +253,9 @@ public interface Arguments {
 	 *
 	 * @since 5.11
 	 * @see Arguments#argumentSet(String, Object...)
-	 * @see org.junit.jupiter.params.ParameterizedTest#ARGUMENT_SET_NAME_PLACEHOLDER
-	 * @see org.junit.jupiter.params.ParameterizedTest#ARGUMENT_SET_NAME_OR_ARGUMENTS_WITH_NAMES_PLACEHOLDER
+	 * @see Arguments#argumentSetFrom(String, Iterable)
+	 * @see org.junit.jupiter.params.ParameterizedInvocationConstants#ARGUMENT_SET_NAME_PLACEHOLDER
+	 * @see org.junit.jupiter.params.ParameterizedInvocationConstants#ARGUMENT_SET_NAME_OR_ARGUMENTS_WITH_NAMES_PLACEHOLDER
 	 */
 	@API(status = MAINTAINED, since = "5.13.3")
 	final class ArgumentSet implements Arguments {
@@ -150,7 +273,8 @@ public interface Arguments {
 
 		/**
 		 * Get the name of this {@code ArgumentSet}.
-		 * @return the name of this {@code ArgumentSet}; never {@code null} or blank
+		 * @return the name of this {@code ArgumentSet}; never {@code null} or
+		 *         blank
 		 */
 		public String getName() {
 			return this.name;
--- jupiter-tests/src/test/java/org/junit/jupiter/params/provider/ArgumentsTests.java
+++ jupiter-tests/src/test/java/org/junit/jupiter/params/provider/ArgumentsTests.java
@@ -10,11 +10,18 @@
 
 package org.junit.jupiter.params.provider;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.junit.jupiter.params.provider.Arguments.of;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -54,6 +61,117 @@ class ArgumentsTests {
 		var arguments = arguments(input);
 
 		assertThat(arguments.get()).isSameAs(input);
+	}
+
+	@Test
+	void fromSupportsCollection() {
+		Collection<@Nullable Object> input = Arrays.asList(1, "two", null, 3.0);
+		var arguments = Arguments.from(input);
+
+		assertArrayEquals(new Object[] { 1, "two", null, 3.0 }, arguments.get());
+	}
+
+	@Test
+	void fromSupportsIterable() {
+		var input = new IterableWithNullableElements(1, "two", null, 3.0);
+		var arguments = Arguments.from(input);
+
+		assertArrayEquals(new Object[] { 1, "two", null, 3.0 }, arguments.get());
+	}
+
+	@Test
+	void fromSupportsListDefensiveCopy() {
+		var input = new ArrayList<@Nullable Object>(asList(1, "two", null, 3.0));
+		var arguments = Arguments.from(input);
+
+		// Modify input
+		input.set(1, "changed");
+		input.add("new");
+
+		// Assert that arguments are unchanged
+		assertArrayEquals(new Object[] { 1, "two", null, 3.0 }, arguments.get());
+	}
+
+	@Test
+	void argumentsFromSupportsCollection() {
+		Collection<@Nullable Object> input = asList("a", 2, null);
+		var arguments = Arguments.argumentsFrom(input);
+
+		assertArrayEquals(new Object[] { "a", 2, null }, arguments.get());
+	}
+
+	@Test
+	void argumentsFromSupportsIterable() {
+		var input = new IterableWithNullableElements("a", 2, null);
+		var arguments = Arguments.argumentsFrom(input);
+
+		assertArrayEquals(new Object[] { "a", 2, null }, arguments.get());
+	}
+
+	@Test
+	void argumentSetSupportsCollection() {
+		Collection<@Nullable Object> input = asList("x", null, 42);
+		var argumentSet = Arguments.argumentSetFrom("list-test", input);
+
+		assertArrayEquals(new Object[] { "x", null, 42 }, argumentSet.get());
+		assertThat(argumentSet.getName()).isEqualTo("list-test");
+	}
+
+	@Test
+	void argumentSetSupportsIterable() {
+		var input = new IterableWithNullableElements("x", null, 42);
+		var argumentSet = Arguments.argumentSetFrom("list-test", input);
+
+		assertArrayEquals(new Object[] { "x", null, 42 }, argumentSet.get());
+		assertThat(argumentSet.getName()).isEqualTo("list-test");
+	}
+
+	@Test
+	void toListReturnsMutableListOfArguments() {
+		var arguments = Arguments.of("a", 2, null);
+
+		var result = arguments.toList();
+
+		assertThat(result).containsExactly("a", 2, null); // preserves content
+		result.add("extra"); // confirms mutability
+		assertThat(result).contains("extra");
+	}
+
+	@Test
+	void toListDoesNotAffectInternalArgumentsState() {
+		var arguments = Arguments.of("a", 2, null);
+
+		var result = arguments.toList();
+		result.add("extra"); // mutate the returned list
+
+		// Confirm that internal state was not modified
+		var freshCopy = arguments.toList();
+		assertThat(freshCopy).containsExactly("a", 2, null);
+	}
+
+	@Test
+	void toListWorksOnEmptyArguments() {
+		var arguments = Arguments.of();
+
+		var result = arguments.toList();
+
+		assertThat(result).isEmpty();
+		result.add("extra");
+		assertThat(result).containsExactly("extra");
+	}
+
+	private static final class IterableWithNullableElements implements Iterable<@Nullable Object> {
+
+		private final Collection<@Nullable Object> collection;
+
+		private IterableWithNullableElements(@Nullable Object... items) {
+			this.collection = asList(items);
+		}
+
+		@Override
+		public Iterator<@Nullable Object> iterator() {
+			return collection.iterator();
+		}
 	}
 
 }
