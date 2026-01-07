4e775a88e4171749194ed87e3d58406d62e80a3f
Marc Philipp <mail@marcphilipp.de>
2026-01-07 08:54:46 +0100

    Use existing static import

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/MediaType.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/MediaType.java
@@ -17,7 +17,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Path;
 
 import org.apiguardian.api.API;
-import org.apiguardian.api.API.Status;
 import org.jspecify.annotations.Nullable;
 import org.junit.platform.commons.PreconditionViolationException;
 import org.junit.platform.commons.util.Preconditions;
@@ -33,7 +32,7 @@ import org.junit.platform.commons.util.Preconditions;
  * @deprecated Use {@link org.junit.jupiter.api.MediaType} instead.
  */
 @Deprecated(since = "5.14", forRemoval = true)
-@API(status = Status.DEPRECATED, since = "5.14")
+@API(status = DEPRECATED, since = "5.14")
 public final class MediaType extends org.junit.jupiter.api.MediaType {
 
 	/**
