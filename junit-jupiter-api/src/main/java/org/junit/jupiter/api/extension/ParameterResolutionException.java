f7120c8627d57011807d6eb4c5a3484bc21ce68c
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2026-03-13 14:09:44 +0100

    Revise nullability of getMessage() in ParameterResolutionException
    Closes #5451

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/ParameterResolutionException.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/ParameterResolutionException.java
@@ -16,6 +16,7 @@ import java.io.Serial;
 
 import org.apiguardian.api.API;
 import org.junit.platform.commons.JUnitException;
+import org.junit.platform.commons.util.Preconditions;
 
 /**
  * Thrown if an error is encountered in the configuration or execution of a
@@ -30,12 +31,37 @@ public class ParameterResolutionException extends JUnitException {
 	@Serial
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Construct a {@code ParameterResolutionException} with the supplied message.
+	 *
+	 * @param message the message; never {@code null}
+	 */
 	public ParameterResolutionException(String message) {
-		super(message);
+		super(Preconditions.notNull(message, "message must not be null"));
 	}
 
+	/**
+	 * Construct a {@code ParameterResolutionException} with the supplied message
+	 * and cause.
+	 *
+	 * @param message the message; never {@code null}
+	 * @param cause the cause; never {@code null}
+	 */
 	public ParameterResolutionException(String message, Throwable cause) {
-		super(message, cause);
+		super(Preconditions.notNull(message, "message must not be null"),
+			Preconditions.notNull(cause, "cause must not be null"));
+	}
+
+	/**
+	 * Get the message, never {@code null}.
+	 */
+	@Override
+	public String getMessage() {
+		String message = super.getMessage();
+		if (message == null) {
+			throw new IllegalStateException("message must not be null");
+		}
+		return message;
 	}
 
 }
